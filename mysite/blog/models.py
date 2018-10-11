f33627e40104e25aeb0fe6dbad7dfd7abab87d69
gabrielascalon <gbscalon@gmail.com>
2018-10-11 01:05:53 -0300

    approve or delete comments by logged in users

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete = models.CASCADE, related_name = 'comments')
     author = models.CharField(max_length=200)
@@ -30,3 +31,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.utils import timezone
 from django.shortcuts import redirect
@@ -70,3 +70,15 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
