3c82c31e77dc8d2be9bc701398b402ad9719efed
gabrielascalon <gbscalon@gmail.com>
2018-10-14 19:01:57 -0300

    added tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from django.urls import reverse
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'teste123'
+        self.user = User.objects.create(username=self.username,
+                                        password=self.password)
+
+    def test_publish_post(self):
+        post = Post(author=self.user, title='test',
+                    text='hello, this is a test post')
+        post.save()
+        post.publish()
+        self.assertTrue(post.published_date != None)
+
+    def test_post_list(self):
+        post = Post(author=self.user,
+                    title='My title for my test post_list',
+                    text='test')
+        post.save()
+        url = reverse('post_list')
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 200)
+        self.assertFalse(post.title in str(response.content))
+        post.publish()
+        response = self.client.get(url)
+        self.assertTrue(post.title in str(response.content))
+
+    def test_post_detail_not_found(self):
+        url = reverse('post_detail', kwargs={'pk': '1234'})
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 404)
+
+    def test_post_detail_draft_not_logged_in(self):
+        post = Post(author=self.user,
+                    title = 'My title for draft not logged in test',
+                    text='test text')
+        post.save()
+        url = reverse('post_detail', kwargs = {'pk': post.pk})
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 404)
+
+    def test_post_detail_draft_logged_in(self):
+        post = Post(author=self.user,
+                    title = 'My title for draft in logged in test',
+                    text='test text')
+        post.save()
+        login = self.client.force_login(self.user)
+        url = reverse('post_detail', kwargs = {'pk': post.pk})
+        response = self.client.get(url)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(post.title in str(response.content))
--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,14 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+    else:
+        post = get_object_or_404(Post.objects.filter(published_date__lte=timezone.now()), pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required
